360aa6fe3a822a14042b33a736fafa20801689f8
copolymer00 <enochjclements@yahoo.com>
2023-02-26 09:21:55 -0600

    Working on fixing UI

--- objects.py
+++ objects.py
@@ -1,68 +1,75 @@
+from enum import Enum
 import datetime
 
 class MowList:
-    _list = list()
-
-    def __init__(self, inpt):
-        self.populate_list(inpt)
-    def populate_list(self, inpt):
-        self._list += inpt.load_customers()
+    def __init__(self, m_list=list()):
+        self.m_list = m_list
     def add_customer(self, cust):
-        if cust not in self._list:
-            self._list += cust
-    def remove_customer(self, name: str):
-        for cust in self._list:
-            if cust.get_name() == name:
-                self._list.pop(cust)
-    #--------------------------------------#
-    def get_list(self):
-        return self._list
+        if cust not in self.m_list:
+            self.m_list += cust
+    def remove_customer(self, idx):
+        del self.m_list[idx]
     def __str__(self):
-        out = '{\n'
-        out += '\tCust. Name:\tJob\tCompletion\n' + '-'*40 + '\n'
-        for cust in self._list:
-            out += '\t' + cust.get_name() + ':\t'
-            for job in cust.get_jobs():
-                out += job.name + '\t' + ('X' if job.complete else 'O') + '\n\t\t\t'
-        out += '\n}'
+        out = "Name\tEmail\tPhone\tJob\tComplete\tBilling"
+        for cust in m_list:
+            for job in cust.jobs:
+                out += "\n" + '\t'.join(cust.info.name,cust.info.email,cust.info.phone,str(job))
         return out
+        ##TODO: return list of customer names
+    def get_customers(self):
+        out = dict()
+        for cust in self.m_list:
+            
+        return dict((cust.name,
+
+class Billing_Status(Enum):
+    NOTSENT = -1
+    SENT = 0
+    PAID = 1
+class Weekday(Enum):
+    Sunday = 1
+    Monday = 2
+    Tuesday = 3
+    Wednesday = 4
+    Thursday = 5
+    Friday = 6
+    Saturday = 7
 
 class Customer:
-    _name = str()
-    _jobs = list()
-    def __init__(self, name, inpt):
-        self._name = name
-        self.populate_jobs(inpt)
+    class Info:
+        def __init__(self, name:str, email:str, phone:str):
+            self.name = name or None
+            self.email = email or None
+            self.phone = phone or None
+    def __init__(self, name:str, jobs:list[Job], email=False, phone=False):
+        self.info = self.Info(name,email,phone)
+        self.jobs = jobs
     def add_job(self, job):
-        self._jobs.append(job)
-    def remove_job(self, name):
-        for job in _jobs:
-            if job.get_name() == name:
-                self._jobs.pop(job)
-    def populate_jobs(self, inpt):
-        self._jobs += inpt.load_jobs(self._name)
-    #--------------------------------#
-    def get_name(self):
-        return self._name
-    def get_jobs(self):
-        return self._jobs
+        self.jobs.append(job)
+    def remove_job(self, idx):
+        del self.jobs[idx]
 
-class Job:
-    name = str()
-    weekly = bool()
-    complete = bool()
-    weekday = int()
-    completion_date = datetime.date
-    hours = float()
-
-    def __init__(self, inpt, name, weekly=True, complete=False, weekday=0):
+class Job_Type:
+    def __init__(self, name:str, weekly:bool, desc:str, tools:list[str]):
         self.name = name
         self.weekly = weekly
-        if self.weekly:
+        self.desc = desc
+        self.tools = tools
+    def to_dict(self):
+        return dict(zip(('name','desc','tools','weekly'),
+                        (self.name,self.desc,self.tools,self.weekly)))
+
+class Job:
+    def __init__(self, j_type:Job_Type, complete:bool, weekday:Weekday, completion_date, billing:Billing_Status, hours):
+        self.j_type = j_type 
+        self.complete = complete
+        self.hours = hours
+
+        if self.j_type.weekly:
             self.weekday = weekday
             if complete:
-                self.completion_date = inpt.get_completion_date()
-                self.hours = inpt.get_job_hours()
-                self.complete = complete ()
+                self.completion_date = completion_date
         else:
-            self.complete = complete
+            self.completion_date = completion_date
+    def __str__(self):
+        return f'{self.j_type.name}\t{"Not Complete" if not complete else completion_date}\t{billing.name}'
